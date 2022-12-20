405dbee39a31663d531ee8ecf5da691a5a306111
MateuszFrankowski <106771721+MateuszFrankowski@users.noreply.github.com>
2022-12-20 11:36:56 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -196,7 +196,7 @@ loadMoreButton.addEventListener('click', () => imageLoader());
 imageLoaderChanger.addEventListener('click', event => {
   if (automaticLoading === true) {
     automaticLoading = false;
-    if (searchLimit === false || amountOfLoadedPIctures==0) {
+    if (searchLimit === false && amountOfLoadedPIctures==0) {
       observator(true);
     } else {
       observator(false);
@@ -204,7 +204,7 @@ imageLoaderChanger.addEventListener('click', event => {
     event.currentTarget.textContent = 'Loading more pictures on button';
   } else {
     automaticLoading = true;
-    if (searchLimit === false || amountOfLoadedPIctures==0) {
+    if (searchLimit === false && amountOfLoadedPIctures==0) {
       observator(true);
     } else {
       observator(false);
